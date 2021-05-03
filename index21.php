a0e2e8179873f3af5d52dc6b400ef324a05fe0a4
Rony Stico <ronystico@gmail.com>
2021-05-03 03:03:22 -0400

    testing git merge

---
+++ index21.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+
+<?php 
+$luckyNumbers = array(4,8,14,16,23,42);
+
+	for ($i = 0;$i < count($luckyNumbers);$i++){
+echo "$luckyNumbers[$i] <br>";
+	}
+
+?>
+
+</body>
+</html>
